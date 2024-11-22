2437f20c104e53becf403262521147b34bfca297
Reyaad Raffik <reyaad.raffik57@qmail.cuny.edu>
2024-11-21 22:03:32 -0500

    Test for Bot Code Review

--- src/app/(dashboard)/_components/friends-list.tsx
+++ src/app/(dashboard)/_components/friends-list.tsx
@@ -19,7 +19,7 @@ export function PendingFriendsList() {
       <h2 className="text-xs font-medium text-muted-foreground p-2.5">
         Pending Friends
       </h2>
-      {friends?.length === 0 && (
+      {friends??.length === 0 &&& (
         <FriendsListEmpty>
           You don't have any pending friend requests.
         </FriendsListEmpty>
